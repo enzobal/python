d268ccac05cb9bff0945cbcd3e8dadc7e7fc2beb
enzobal <121642073+enzobal@users.noreply.github.com>
2023-05-03 00:27:19 -0300

    ambos
    am

--- mi_primer_archivo.py
+++ mi_primer_archivo.py
@@ -200,9 +200,91 @@
 
 # semana 3
 
-i=1
-i+=1
-while i<= 5:
-    print(i)
+# i=1
+# i+=1
+# while i<= 5:
+#     print(i)
     
+#     i+=1
+
+
+
+# ejercicios  de la semana 3
+
+
+# 1-Escribe un programa que pida al usuario una palabra y luego imprima cada
+# letra de la palabra en una línea separada.
+
+# palabra = input( "escribe una palabra ")
+# for letra in palabra:
+#     print(letra)
+    
+    
+# 2-Escribe un programa que pida al usuario un número y calcule la suma de todos
+# los números naturales del 1 hasta ese número.
+
+
+numero = int(input(" escribe un numero "))
+i= 1
+while i<= numero:
+    print(i)
     i+=1
+    e={"x":[i+=1]}
+    d=i + e["x"]
+    print(d)
+
+
+# 3-Escribe un programa que pida al usuario un número y luego imprima la tabla de
+# multiplicar correspondiente a ese número del 1 al 10.
+# 4-Escribe un programa que imprima los números pares del 1 al 100.
+# 5-Escribe un programa que imprima la suma de todos los números pares del 1 al
+# 100.
+# 6-Escribe un programa que pida al usuario una palabra y luego imprima la misma
+# palabra pero con las letras en orden inverso.
+# 7-Escribe un programa que pida al usuario una palabra y determine si es un
+# palíndromo (es decir, si se lee igual de izquierda a derecha que de derecha a
+# izquierda).
+# 8-Escribe un programa que pida al usuario una cadena de texto y luego imprima
+# el número de palabras que contiene.
+# 9-Escribe un programa que pida al usuario un número y luego imprima la
+# secuencia de Fibonacci correspondiente a ese número.
+# 10-Escribe un programa que pida al usuario una cadena de texto y luego imprima
+# la misma cadena pero con todas las vocales en mayúscula.
+# 11-Escribe un programa que pida al usuario un número y calcule su factorial.
+# Un factorial es el producto que resulta de multiplicar un número entero positivo
+# dado por todos los enteros inferiores a él hasta el uno. Por ejemplo, el factorial
+# de 4 es 4! = 4 × 3 × 2 × 1 = 24.
+# 12-Escribe un programa que pida al usuario una lista de números separados por
+# comas y calcule su promedio.
+# 13-Escribe un programa que pida al usuario un número y luego imprima un
+# triángulo de asteriscos con esa cantidad de filas.
+# *
+# **
+# ***
+# ****
+# *****
+# 14-Escribe un programa que pida al usuario un número y luego imprima un
+# triángulo de números como el siguiente:
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+# 15-Escribe un programa que pida al usuario una cadena de texto y determine
+# cuántas veces aparece cada letra en la cadena.
+# 16-Escribe un programa que pida al usuario una cadena de texto y luego imprima
+# la misma cadena pero con cada palabra al revés.
+# 17-Escribe un programa que pida al usuario una cadena de texto y luego imprima
+# la misma cadena pero con las palabras en orden inverso.
+# 18-Escribe un programa que pida al usuario un número y luego imprima un
+# triángulo de números como el siguiente:
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 19-Escribe un programa que pida al usuario un número y luego imprima si ese
+# número es un número perfecto o no. Un número perfecto es aquel que es igual a
+# la suma de sus divisores propios (excluyendo el propio número).
+# Los números perfectos son aquellos iguales a la suma de sus
+# divisores: 6 se
+# puede dividir por 1, 2 y 3, y cuando sumas esos números, el resultado es 6
